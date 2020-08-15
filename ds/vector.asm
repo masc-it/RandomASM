d03c9a5713a09918927784f9d0c00d791a54c61d
onipot <m.sciancalepore20@studenti.uniba.it>
2020-08-15 11:37:27 +0200

    vector v2: ora si che si può andare in campagna

--- ds/vector.asm
+++ ds/vector.asm
@@ -4,9 +4,10 @@ section .data
     chunk EQU 128
     threshold EQU 24
 
+    current_offset dq 0
     size dq 0
 
-    printf_1 db "%d", 10,0
+    printf_1 db "%c", 10,0
 section .bss
 
 section .text
@@ -21,87 +22,49 @@ _start:
     mov rbp, rsp
     sub rsp, 16
 
+    ; alloco memoria iniziale
     call alloc_vector
-
     mov [rsp+.vector_addr], rax
 
-    ; copio la dimensione attuale
-    mov r8, [size]
+    ; PRIMO ELEMENTO
 
-    ; calcolo offset
-    mov rax, r8
-    mov rbx, element_size
-    mul rbx
-
-    mov rdx, rax
-
-    ; debug
-
-    ;xor rax, rax
-    ;mov al, 125
-
-    ; controllo se ci sono almeno 24 byte (3 qword) liberi
-    mov r9, [actual_size]
-    mov rbx, threshold
-    sub r9, rdx ; byte rimanenti
-    cmp r9, rbx
-    jg .continue ; non reallocare se > threshold
+    ; controllo disponibilità memoria
     mov rdi, [rsp+.vector_addr]
-    push r8
-    push rdx
+    call check_size
 
-    call realloc_vector
-
-    pop rdx
-    pop r8
-
-
-     mov [rsp+.vector_addr], rax
-
-
-
-      .continue:
-
-    ; rdx conterrà l'offset
-
-
-    ; carico elemento da inserire
+    ; aggiungo elemento
+    mov rdi, [rsp+.vector_addr]
     xor rax, rax
     mov al, 65
+    mov rsi, rax
+    call add_element
+
+    ; stampo
+    lea rdi, [printf_1]
+    mov rdx, [rsp+.vector_addr]
+    mov rsi, qword[rdx]
+    xor rax, rax
+    call printf
+
+
+    ; SECONDO ELEMENTO
+    mov rdi, [rsp+.vector_addr]
+    call check_size
 
     mov rdi, [rsp+.vector_addr]
-    add rdi, rdx
-    mov [rdi], qword rax
-
-    ; size = size + 1
-    inc r8
-    mov [size], r8
-
-
-    ; 2 elemento
-    mov r8, [size]
-    ; calcolo offset
-    mov rax, r8
-    mov rbx, element_size
-    mul rbx
-
-    ; rdx conterrà l'offset
-    mov rdx, rax
-    
-    ; carico elemento da inserire
     xor rax, rax
     mov al, 66
+    mov rsi, rax
+    call add_element
 
-    mov rdi, [rsp+.vector_addr]
-    add rdi, rdx
-    mov [rdi], qword rax
+    ; todo sposta check_size in add_element
 
-
-    ; size = size + 1
-    inc r8
-    mov [size], r8
-
-
+    lea rdi, [printf_1]
+    mov rdx, [rsp+.vector_addr]
+    add rdx, element_size
+    mov rsi, qword[rdx]
+    xor rax, rax
+    call printf
 
 
 
@@ -132,6 +95,62 @@ realloc_vector:
     add rsi, rdx
     mov [actual_size], rsi
     call realloc
+
+    leave
+    ret
+
+
+check_size:
+
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8
+
+    ; copio la dimensione attuale
+    mov r8, [size]
+
+    ; calcolo offset
+    mov rax, r8
+    mov rbx, element_size
+    mul rbx
+
+    mov rdx, rax
+
+    mov rax, rdi ; return
+    mov [current_offset], rdx
+    ; debug
+
+    ;xor rax, rax
+    ;mov al, 125
+
+    ; controllo se ci sono almeno 24 byte (3 qword) liberi
+    mov r9, [actual_size]
+    mov rbx, threshold
+    sub r9, rdx ; byte rimanenti
+    cmp r9, rbx
+    jg fin ; non reallocare se > threshold
+
+    call realloc_vector
+    fin:
+    leave
+    ret
+
+
+add_element:
+    ; rdi = indirizzo
+    ; rsi = elemento da inserire
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8
+    ;mov rdi, [rsp+.vector_addr]
+    mov rax, [current_offset]
+    add rdi, rax
+    mov [rdi], qword rsi
+
+    ; size = size + 1
+    mov r8, [size]
+    inc r8
+    mov [size], r8
 
     leave
     ret
